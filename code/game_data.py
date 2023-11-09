5d71364a325dbaebb73fccfdb46c37f343b85856
DeXils <angel.gioanni@gmail.com>
2023-11-09 04:51:28 +0100

    Add 2 level

--- code/game_data.py
+++ code/game_data.py
@@ -5,3 +5,19 @@ level_1 = {
     'player': '../levels/1/level1_player.csv',
     'enemies': '../levels/1/level1_enemies.csv'
 }
+
+level_2 = {
+    'ground' : '../levels/2/level2_ground.csv',
+    'doors' : '../levels/2/level2_doors.csv',
+    'walls' : '../levels/2/level2_walls.csv',
+    'player': '../levels/2/level2_player.csv',
+    'enemies': '../levels/2/level2_enemies.csv'
+}
+
+level_3 = {
+    'ground' : '../levels/3/level3_ground.csv',
+    'doors' : '../levels/3/level3_doors.csv',
+    'walls' : '../levels/3/level3_walls.csv',
+    'player': '../levels/3/level3_player.csv',
+    'enemies': '../levels/3/level3_enemies.csv'
+}
